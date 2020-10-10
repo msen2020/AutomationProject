07b8b17a03994f446bed6b0a012ddacf8d7caa24
EU3 Batch GitHup <msen2020@hotmail.com>
2020-10-11 01:09:59 +0200

    I have updated the Hello.java file

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,11 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Class");
+        System.out.println("Hello Class EU3");
+
+        //MORE CHANGES ARE COMING
+        // I am ready to commit this
+
 
     }
 }
